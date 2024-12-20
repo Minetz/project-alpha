86efd5ac4cf9e9c0a79a05b80a450791b0c3840e
Niccolò Howard Minetti <nhminetti@gmail.com>
2024-12-20 21:45:55 +0100

    chore: moved to toml for cc.

--- src/config/load_env.py
+++ src/config/load_env.py
@@ -1,6 +1,6 @@
-import os
-from dotenv import load_dotenv
+import tomllib
 
 def load_api_key():
-    load_dotenv()
-    return os.getenv('OPENAI_API_KEY')
+    with open("./src/config/config.toml", "rb") as f:
+        config = tomllib.load(f)
+    return config["openai"]["api_key"]
