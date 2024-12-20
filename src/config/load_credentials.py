f7fdc566ee8d0b46159a79c0a06c1d46abdb4163
Niccolò Howard Minetti <nhminetti@gmail.com>
2024-12-20 22:23:15 +0100

    fix: crendtials

--- src/config/load_credentials.py
+++ src/config/load_credentials.py
@@ -2,7 +2,7 @@ import tomllib
 import os
 
 def load_credentials():
-    config_path = os.path.join(os.path.dirname(__file__), "config.toml")
+    config_path = os.path.join(os.path.dirname(__file__), ".streamlit/secrets.toml")
     with open(config_path, "rb") as f:
         config = tomllib.load(f)
     username = config["credentials"]["username"]
